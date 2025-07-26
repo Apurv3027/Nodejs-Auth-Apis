0c2647372304cc19fe82ccea816bafc10f039732
Apurv Patel <apurvp002@gmail.com>
2025-07-27 01:08:04 +0530

    Update database connection

--- db/connection.js
+++ db/connection.js
@@ -16,6 +16,7 @@ const connectDB = async () => {
             // Additional options for Atlas connection
             serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
             socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+            bufferCommands: false, // Disable mongoose buffering
         });
 
         console.log(`MongoDB Atlas connected successfully: ${conn.connection.host}`);

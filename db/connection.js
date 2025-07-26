11a8346f377394f659e253ba03eaa909b4415e4e
Apurv Patel <apurvp002@gmail.com>
2025-07-27 00:40:48 +0530

    Add to production server

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,6 @@
-const mongoose = require('mongoose');
 require('dotenv').config();
+
+const mongoose = require('mongoose');
 
 // MongoDB Atlas cluster connection
 const MONGODB_ATLAS_URI = process.env.MONGODB_ATLAS_URI;
